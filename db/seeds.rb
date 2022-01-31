73d1e3b0c1e759d4fb0afa2662e256ab6ec2730e
Willand <dadiawilland@gmail.com>
2022-01-31 14:53:51 +0800

    jeremy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+employee = Employee.create({ name: 'Alnwkr', first_name: 'Allen', last_name: 'Mendoza' })
