a657b2e441cd02d8327078f03c631cf452827542
Willand <dadiawilland@gmail.com>
2022-01-31 14:46:58 +0800

    christian

--- db/migrate/20220131064146_add_attributes_to_employee.rb
+++ db/migrate/20220131064146_add_attributes_to_employee.rb
@@ -1,4 +1,6 @@
 class AddAttributesToEmployee < ActiveRecord::Migration[6.0]
   def change
+    add_column :employees,  :first_name, :string, null: true
+    add_column :employees,  :last_name, :string, null: true
   end
 end
