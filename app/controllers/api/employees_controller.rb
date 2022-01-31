cdb30e95ff9b7377acb69b5a7031ff8a300f4707
Willand <dadiawilland@gmail.com>
2022-01-31 15:24:03 +0800

    willand

--- app/controllers/api/employees_controller.rb
+++ app/controllers/api/employees_controller.rb
@@ -1,30 +1,38 @@
-module Api
-    class EmployeesController < ApplicationController
-        before_action :set_employee, only: %i[show update]
+class Api::EmployeesController < ApplicationController
+    before_action :set_employee, only: %i[show update destroy]
 
-        def index
-            employees = Employee.all
-            render json: employees
+    def index
+        employees = Employee.all
+        render json: employees
+    end
+
+    def create
+        employee = Employee.new(employee_params)
+        employee.save
+        render json: employee
+    end
+
+    def show
+        render json: @employee
+    end
+
+    def update
+        @employee.update(employee_params)
+        render json: @employee
+    end
+
+    def destroy
+        @employee.destroy
+        render json: "allend walker destroyed"
+    end
+
+    private
+
+    def set_employee
+        @employee = Employee.find(params[:id])
         end
 
-        def create
-            employee = Employee.new(employee_params)
-            employee.save
-            render json: employee
-        end
-
-        def show
-            
-        end
-
-        private
-
-        def set_employee
-            @employee = Employee.find(params[:id])
-         end
-
-        def employee_params
-            params.permit(:name, :first_name, :last_name)
-        end
+    def employee_params
+        params.permit(:name, :first_name, :last_name)
     end
 end
