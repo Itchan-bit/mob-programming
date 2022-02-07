a6a83b5cace91163de890121eff5ac6396754547
Willand <dadiawilland@gmail.com>
2022-02-07 15:08:50 +0800

    marc

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  authenticated :user do
+    root "users#index", as: :authenticated_root
+  end
+  root 'users#index'
 end
