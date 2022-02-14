b59dd56743a8cd2ae0c1812c8267173b74368697
Grenarch <grenarch@Grenarchs-MacBook-Pro.local>
2022-02-14 15:08:47 +0800

    James

---
+++ app/javascript/components/navigation/Header.jsx
@@ -0,0 +1,14 @@
+// app/javascript/packs/components/TodoApp.jsx
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = () => {
+    return (
+        <nav>
+            <h1>Users list</h1>
+            <button>Create User</button>
+        </nav>
+    )
+}
+
+export default Header;
