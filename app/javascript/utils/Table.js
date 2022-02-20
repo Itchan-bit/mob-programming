3343291d81a33f520c14ad045c8f042199f9d9f4
Grenarch <grenarch@Grenarchs-MacBook-Pro.local>
2022-02-20 19:27:43 +0800

    Add a action button to the table

---
+++ app/javascript/utils/Table.js
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
+import { getUser } from "../services/userApi";
+import { UserColumns } from "../constant/Columns";
+
+export default function StickyHeadTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [users, setUsers] = useState([]);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const constructData = (email, contact_number, department) => {
+    return { email, contact_number, department };
+  };
+
+  useEffect(() => {
+    const mount = true;
+    getUser().then((res) => {
+      if (mount) {
+        const datas = [];
+        res.data.map((data) => {
+          datas.push(
+            constructData(data.email, data.contact_number, data.department)
+          );
+        });
+
+        setUsers(datas);
+      }
+    });
+
+    return () => (mount = false);
+  }, []);
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {UserColumns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {UserColumns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.id === "action" ? (
+                            <Box sx={{ "& button": { m: 1 } }}>
+                              <Button
+                                variant="outlined"
+                                size="small"
+                                color="primary"
+                              >
+                                Update
+                              </Button>
+                              <Button
+                                variant="outlined"
+                                size="small"
+                                color="error"
+                              >
+                                Delete
+                              </Button>
+                            </Box>
+                          ) : (
+                            value
+                          )}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={users.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
